63ab01d5bbe71c5140f2e1075d814066ebb15bcb
Godswill Udida <udidagodswill7@gmail.com>
2023-09-25 17:20:58 +0100

    from udida

--- router/checkoutRouter.ts
+++ router/checkoutRouter.ts
@@ -1,6 +1,9 @@
-import {Router} from "express"
+import Router from "express"
+import { payment } from "../controller/checkoutController"
 
 const router = Router()
+
+router.route("/paymemt").post(payment)
 
 
 export default router
