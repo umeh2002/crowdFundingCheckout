1722e5324b456e382dc9db0f486b5631888c2628
Oscdivin <divineuwandu84@Gmail.com>
2023-09-27 12:08:11 +0100

    osinachi

--- router/checkoutRouter.ts
+++ router/checkoutRouter.ts
@@ -6,7 +6,7 @@ import { checkOutWithPayStack } from "../controller/checkoutController"
 
 const router = express.Router()
 
-router.route("/:Begid/pay-with-wallet").post(checkOut,payWithWallet)
-router.route("/:Begid/pay-with-PayStack").post(checkOutWithPayStack)
+router.route("/:abegID/pay-with-wallet").post(checkOut,payWithWallet)
+router.route("/:abegID/pay-with-PayStack").post(checkOutWithPayStack)
 
 export default router
