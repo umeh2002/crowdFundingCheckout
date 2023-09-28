37ffd914fda02b260b4ef6645ba004b3614e3a37
umeh emmanuel <eumeh3882@gmail.com>
2023-09-28 06:47:02 -0700

    emma

--- controller/checkoutController.ts
+++ controller/checkoutController.ts
@@ -1,56 +1,54 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 import https from "https";
 import { HTTP } from "../Error/mainError";
 
-
 export const checkOutWithPayStack = async (req: Request, res: Response) => {
-    try {
-        const { email, amount } = req.body;
-        // const {abegID} = req.params
+  try {
+    const { amount } = req.body;
+    // const {abegID} = req.params
 
-            const params = JSON.stringify({
-                email: email,
-                amount: amount * 100,
-                // abegID
-            });
-    
-            const options = {
-                hostname: "api.paystack.co",
-                port: 443,
-                path: "/transaction/initialize",
-                method: "POST",
-                headers: {
-                    Authorization:
-                        "Bearer sk_test_ec1b0ccabcb547fe0efbd991f3b64b485903c88e",
-                    "Content-Type": "application/json",
-                },
-            };
-    
-            const ask = https
-                .request(options, (resp) => {
-                    let data = "";
-                    resp.on("data", (chunk) => {
-                        data += chunk;
-                    });
-    
-                    resp.on("end", () => {
-                        console.log(JSON.parse(data));
-                        res.status(HTTP.OK).json({
-                            message: "Payment successful",
-                            data: JSON.parse(data),
-                        });
-                    });
-                })
-                .on("error", (error) => {
-                    console.error(error);
-                });
-    
-            ask.write(params);
-            ask.end();
-       
-    } catch (error) {
-        return res.status(HTTP.BAD_REQUEST).json({
-            message: "Error making Payment",
+    const params = JSON.stringify({
+      email: "customer@email.com",
+      amount: amount * 100,
+      // abegID
+    });
+
+    const options = {
+      hostname: "api.paystack.co",
+      port: 443,
+      path: "/transaction/initialize",
+      method: "POST",
+      headers: {
+        Authorization:
+          "Bearer sk_test_ec1b0ccabcb547fe0efbd991f3b64b485903c88e",
+        "Content-Type": "application/json",
+      },
+    };
+
+    const ask = https
+      .request(options, (resp) => {
+        let data = "";
+        resp.on("data", (chunk) => {
+          data += chunk;
         });
-    }
+
+        resp.on("end", () => {
+          console.log(JSON.parse(data));
+          res.status(HTTP.OK).json({
+            message: "Payment successful",
+            data: JSON.parse(data),
+          });
+        });
+      })
+      .on("error", (error) => {
+        console.error(error);
+      });
+
+    ask.write(params);
+    ask.end();
+  } catch (error) {
+    return res.status(HTTP.BAD_REQUEST).json({
+      message: "Error making Payment",
+    });
+  }
 };
