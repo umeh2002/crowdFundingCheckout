90f77985e9be7581d154d4d556bd37c835bc411c
umeh emmanuel <eumeh3882@gmail.com>
2023-10-03 10:06:37 -0700

    fixed

--- controller/walletController.ts
+++ controller/walletController.ts
@@ -1,8 +1,12 @@
 import { Response } from "express";
 import {HTTP} from "../Error/mainError"
 import { PrismaClient } from "@prisma/client";
+import axios from "axios"
+import { publishConnection } from "../utils/connection";
 
 const prisma = new PrismaClient()
+
+const URL:string ="https://crowded-auth.onrender.com"
 
 export const payWithWallet = async (req : any, res : Response) =>{
     try {
@@ -10,16 +14,40 @@ export const payWithWallet = async (req : any, res : Response) =>{
         const {abegID} = req.params
         const { email, note, name, amount } = req.body
 
+        const options={
+            method:"GET",
+            url:`${URL}/api/${id}/single-account`
+        }
+       
+
+        const wallet:any =await axios.request(options).then((res) => {
+            return res.data.data
+        })
+
+        console.log(wallet.profile.walletBalance)
+
+        console.log("name",name)
+
+        console.log("email",email)
+
+        console.log(typeof amount)
+
+           if (wallet.profile.walletBalance > amount) {
             const payment = await prisma.crowdCheckOut.create({
                 data : {
-                    email, note, name, amount, abegID, userID :id
+                    email, note, name, amount:parseInt(amount), abegID, userID :id
                 }
             })
-    
+            publishConnection("checkouted", payment)
             return res.status(HTTP.OK).json({
                 message : "Payed Successfully",
                 data : payment
             })
+           } else {
+            return res.status(HTTP.BAD_REQUEST).json({
+                message : "insufficient funds",
+            })
+           }
 
     } catch (error : any) {
         return res.status(HTTP.BAD_REQUEST).json({
--- dist/controller/walletController.js
+++ dist/controller/walletController.js
@@ -8,25 +8,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.payWithWallet = void 0;
 const mainError_1 = require("../Error/mainError");
 const client_1 = require("@prisma/client");
+const axios_1 = __importDefault(require("axios"));
+const connection_1 = require("../utils/connection");
 const prisma = new client_1.PrismaClient();
+const URL = "https://crowded-auth.onrender.com";
 const payWithWallet = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { id } = req.user;
         const { abegID } = req.params;
         const { email, note, name, amount } = req.body;
-        const payment = yield prisma.crowdCheckOut.create({
-            data: {
-                email, note, name, amount, abegID, userID: id
-            }
+        const options = {
+            method: "GET",
+            url: `${URL}/api/${id}/single-account`
+        };
+        const wallet = yield axios_1.default.request(options).then((res) => {
+            return res.data.data;
         });
-        return res.status(mainError_1.HTTP.OK).json({
-            message: "Payed Successfully",
-            data: payment
-        });
+        console.log(wallet.profile.walletBalance);
+        console.log("name", name);
+        console.log("email", email);
+        console.log(typeof amount);
+        if (wallet.profile.walletBalance > amount) {
+            const payment = yield prisma.crowdCheckOut.create({
+                data: {
+                    email, note, name, amount: parseInt(amount), abegID, userID: id
+                }
+            });
+            (0, connection_1.publishConnection)("checkouted", payment);
+            return res.status(mainError_1.HTTP.OK).json({
+                message: "Payed Successfully",
+                data: payment
+            });
+        }
+        else {
+            return res.status(mainError_1.HTTP.BAD_REQUEST).json({
+                message: "insufficient funds",
+            });
+        }
     }
     catch (error) {
         return res.status(mainError_1.HTTP.BAD_REQUEST).json({
